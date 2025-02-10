2a3982b30982c37712f1fbbff44dbd697e4ed443
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-02-10 22:59:58 +0600

    home initial view done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,185 +1,14 @@
-//------------------------------------------normal
-// import { Link } from "react-router-dom";
-// //import CallToAction from "../components/CallToAction";
-// import { useEffect, useState } from "react";
-// import PostCard from "../components/PostCard";
-
-// export default function Home() {
-//   const [posts, setPosts] = useState([]);
-
-//   useEffect(() => {
-//     const fetchPosts = async () => {
-//       const res = await fetch("/api/post/getPosts");
-//       const data = await res.json();
-//       setPosts(data.posts);
-//     };
-//     fetchPosts();
-//   }, []);
-//   return (
-//     <div>
-//       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
-//         <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
-//         <p className="text-gray-500 text-xs sm:text-sm">
-//           Here you&apos;ll find a variety of articles and tutorials on topics
-//           such as web development, software engineering, and programming
-//           languages.
-//         </p>
-//         <Link
-//           to="/search"
-//           className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
-//         >
-//           View all posts
-//         </Link>
-//       </div>
-//       {/* <div className="p-3 bg-amber-100 dark:bg-slate-700">
-//         <CallToAction />
-//       </div> */}
-
-//       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
-//         {posts && posts.length > 0 && (
-//           <div className="flex flex-col gap-6">
-//             <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-//             <div className="flex flex-wrap gap-4">
-//               {posts.map((post) => (
-//                 <PostCard key={post._id} post={post} />
-//               ))}
-//             </div>
-//             <Link
-//               to={"/search"}
-//               className="text-lg text-teal-500 hover:underline text-center"
-//             >
-//               View all posts
-//             </Link>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-//----------------------------------------------------------------------------normal slider
-
-// import { useRef, useState, useEffect } from "react";
-// import { ChevronDown, ChevronLeft, ChevronRight } from "lucide-react";
-// import PostCard from "../components/PostCard";
-
-// function Home() {
-//   const [posts, setPosts] = useState([]);
-//   const [currentIndex, setCurrentIndex] = useState(0); // Track the current index for the posts to show
-
-//   useEffect(() => {
-//     const fetchPosts = async () => {
-//       const res = await fetch("/api/post/getPosts");
-//       const data = await res.json();
-//       setPosts(data.posts); // Get all posts, we'll display them in batches of 3
-//     };
-//     fetchPosts();
-//   }, []);
-
-//   const mainContentRef = useRef(null);
-
-//   const handleExplore = () => {
-//     if (mainContentRef.current) {
-//       mainContentRef.current.scrollIntoView({ behavior: "smooth" });
-//     }
-//   };
-
-//   // Scroll functions to go through posts
-//   const handleScrollLeft = () => {
-//     setCurrentIndex((prevIndex) =>
-//       prevIndex === 0 ? Math.max(0, posts.length - 3) : prevIndex - 3
-//     );
-//   };
-
-//   const handleScrollRight = () => {
-//     setCurrentIndex((prevIndex) =>
-//       prevIndex + 3 >= posts.length ? 0 : prevIndex + 3
-//     );
-//   };
-
-//   return (
-//     <div className="text-violet-700/50 dark:text-white/60">
-//       {/* Initial View */}
-//       <div className="h-screen relative bg-gradient-to-b from-violet-300 to-violet-100 dark:from-[#4f4d4d] dark:via-[#18181b] dark:to-violet-800 pointer-events-none">
-//         <div
-//           className="absolute inset-0 z-0 opacity-70 bg-[url('/gif.gif')] dark:bg-[url('/gif_dark.gif')]"
-//           style={{
-//             // backgroundImage:
-//             //   'url("https://i.pinimg.com/originals/b8/ba/79/b8ba798cc393f599e41b7d2a52ea0ee2.gif")',
-//             backgroundSize: "cover",
-//             backgroundPosition: "center",
-//           }}
-//         />
-//         <div className="absolute top-0 left-0 z-10 p-8 text-left">
-//           <h1 className="text-9xl font-light tracking-wider">Harmonia</h1>
-//           <div className="flex justify-center mt-4">
-//             <button
-//               onClick={handleExplore}
-//               className="flex flex-col items-center gap-2 group transition-all duration-300 hover:opacity-80 pointer-events-auto"
-//             >
-//               <span className="text-sm tracking-widest">EXPLORE</span>
-//               <ChevronDown className="w-6 h-6 animate-bounce" />
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-
-//       {/* Main Content */}
-//       <section
-//         ref={mainContentRef}
-//         className="min-h-screen bg-violet-50 dark:bg-[#18181b] relative"
-//       >
-//         {/* Blog Posts */}
-//         <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
-//           <div className="flex flex-col gap-6">
-//             <h2 className="text-3xl font-light mt-10 text-violet-700 text-center">
-//               Recent Posts
-//             </h2>
-//             <div className="flex justify-center items-center gap-4">
-//               {/* Left Chevron */}
-//               <button
-//                 onClick={handleScrollLeft}
-//                 className="p-2 bg-violet-500 text-white rounded-full hover:bg-violet-600"
-//               >
-//                 <ChevronLeft className="w-6 h-6" />
-//               </button>
-
-//               {/* Post Cards */}
-//               <div className="flex overflow-hidden w-full p-4 justify-center gap-4">
-//                 {posts.slice(currentIndex, currentIndex + 3).map((post) => (
-//                   <PostCard key={post._id} post={post} />
-//                 ))}
-//               </div>
-
-//               {/* Right Chevron */}
-//               <button
-//                 onClick={handleScrollRight}
-//                 className="p-2 bg-violet-500 text-white rounded-full hover:bg-violet-600"
-//               >
-//                 <ChevronRight className="w-6 h-6" />
-//               </button>
-//             </div>
-
-//             {posts.length === 0 && (
-//               <p className="text-center text-gray-400">No posts available.</p>
-//             )}
-
-//             <a
-//               href="/search"
-//               className="text-lg text-violet-500 hover:underline text-center"
-//             >
-//               View all posts
-//             </a>
-//           </div>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// }
-
-// export default Home;
-
 import { useRef, useState, useEffect } from "react";
-import { ChevronDown, ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  Newspaper,
+  Music2,
+  Shuffle,
+  Lightbulb,
+  ChevronsDown,
+} from "lucide-react";
 import { AnimatePresence, motion } from "framer-motion";
 import PostCard from "../components/PostCard";
 
@@ -196,11 +25,31 @@ function Home() {
     fetchPosts();
   }, []);
 
-  const mainContentRef = useRef(null);
+  const blogRef = useRef(null);
+  const handleExploreBlog = () => {
+    if (blogRef.current) {
+      blogRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-  const handleExplore = () => {
-    if (mainContentRef.current) {
-      mainContentRef.current.scrollIntoView({ behavior: "smooth" });
+  const lyricRef = useRef(null);
+  const handleExploreLyric = () => {
+    if (lyricRef.current) {
+      lyricRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const playlistRef = useRef(null);
+  const handleExplorePlaylist = () => {
+    if (playlistRef.current) {
+      playlistRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const suggestionRef = useRef(null);
+  const handleExploreSuggestion = () => {
+    if (suggestionRef.current) {
+      suggestionRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -220,36 +69,97 @@ function Home() {
   return (
     <div>
       {/* Initial View */}
-      <div className="h-screen relative bg-gradient-to-b from-violet-300 to-violet-100 dark:from-[#4f4d4d] dark:via-[#18181b] dark:to-violet-800 pointer-events-none">
+      <div className="h-screen relative bg-gradient-to-b from-violet-300 to-violet-100 dark:from-[#18181b] dark:via-[#18181b] dark:to-violet-800 pointer-events-none">
         <div
-          className="absolute inset-0 z-0 opacity-70 bg-[url('/gif.gif')] dark:bg-[url('/gif_dark.gif')]"
+          className="absolute inset-0 z-0 opacity-60 dark:opacity-50 bg-[url('/home.png')] dark:bg-[url('/home_dark.png')]"
           style={{
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         />
-        <div className="absolute top-0 left-0 z-10 p-8 text-left">
-          <h1 className="text-9xl font-light tracking-wider dark:text-white text-violet-500">
+        <div className="p-8 text-center">
+          <h1 className="text-[120px] -mt-[30px] font-thin tracking-wider dark:text-white/80 dark:mix-blend-screen text-violet-700/60 mix-blend-darken">
             Harmonia
           </h1>
           <div className="flex justify-center mt-4">
-            <button
-              onClick={handleExplore}
-              className="flex flex-col items-center gap-2 group transition-all duration-300 hover:opacity-80 pointer-events-auto"
-            >
-              <span className="text-sm tracking-widest text-violet-500 dark:text-white">
+            <button className="-mt-[20px] flex flex-col items-center gap-2 group transition-all duration-300 hover:opacity-80">
+              <span className="text-sm tracking-widest dark:text-violet-400 dark:mix-blend-screen text-black/60 mix-blend-darken">
                 EXPLORE
               </span>
-              <ChevronDown className="w-6 h-6 animate-bounce text-violet-500 dark:text-white" />
+              <ChevronDown className="w-6 h-6 animate-pulse text-black/60 dark:text-violet-300" />
             </button>
+          </div>
+          {/* Feature Buttons Container */}
+          <div className="flex justify-center gap-10 mt-6 pointer-events-auto">
+            <div className="relative group">
+              <button
+                onClick={handleExploreBlog}
+                className="px-5 py-5 bg-gradient-to-tr from-transparent to-violet-500 rounded-lg shadow-lg hover:scale-105 hover:opacity-80 duration-300"
+              >
+                <Newspaper className="w-8 h-8 text-violet-700/50 dark:text-violet-500 dark:mix-blend-screen mix-blend-multiply" />
+              </button>
+              <div className="absolute left-1/2 transform -translate-x-1/2 top-full -mt-[30px] hidden group-hover:block">
+                <div className="relative bg-transparent text-violet-800 font-thin dark:text-violet-100 px-3 py-1.5 text-sm rounded-sm border border-violet-300 dark:border-violet-700 shadow-lg">
+                  <div className="absolute left-1/2 transform -translate-x-1/2 -top-1 w-2 h-2 bg-violet-200 dark:bg-violet-900 border-t border-l border-violet-400 dark:border-violet-700 rotate-45"></div>
+                  Blog
+                </div>
+              </div>
+              <ChevronsDown className="ml-6 mt-2 text-violet-700/50 dark:text-violet-500 animate-bounce" />
+            </div>
+
+            <div className="mt-10 relative group">
+              <button
+                onClick={handleExploreLyric}
+                className="px-5 py-5 bg-gradient-to-tr from-transparent to-violet-500 rounded-lg shadow-lg hover:scale-105 hover:opacity-80 duration-300"
+              >
+                <Music2 className="w-8 h-8 text-rose-300 mix-blend-multiply dark:mix-blend-screen" />
+              </button>
+              <div className="absolute left-1/2 transform -translate-x-1/2 top-full mt-2 hidden group-hover:block">
+                <div className="relative bg-transparent text-rose-800 font-thin dark:text-rose-100 px-3 py-1.5 text-sm rounded-sm border border-rose-300 dark:border-rose-700 shadow-lg">
+                  <div className="absolute left-1/2 transform -translate-x-1/2 -top-1 w-2 h-2 bg-rose-200 dark:bg-rose-900 border-t border-l border-rose-400 dark:border-rose-700 rotate-45"></div>
+                  <span className="text-xs">Lyric-Card</span>
+                </div>
+              </div>
+              <ChevronDown className="ml-6 mt-2 text-rose-300 animate-bounce" />
+            </div>
+            <div className="mt-10 relative group">
+              <button
+                onClick={handleExplorePlaylist}
+                className="px-5 py-5 bg-gradient-to-tr from-transparent to-violet-500 rounded-lg shadow-lg hover:scale-105 hover:opacity-80 duration-300"
+              >
+                <Shuffle className="w-8 h-8 text-blue-400 mix-blend-multiply dark:mix-blend-screen" />
+              </button>
+              <div className="absolute left-1/2 transform -translate-x-1/2 top-full mt-2 hidden group-hover:block">
+                <div className="relative bg-transparent text-blue-800 font-thin dark:text-blue-100 px-3 py-1.5 text-sm rounded-sm border border-blue-300 dark:border-blue-700 shadow-lg">
+                  <div className="absolute left-1/2 transform -translate-x-1/2 -top-1 w-2 h-2 bg-blue-200 dark:bg-blue-900 border-t border-l border-blue-400 dark:border-blue-700 rotate-45"></div>
+                  <span className="text-xs">Mood-Based Playlist</span>
+                </div>
+              </div>
+              <ChevronDown className="ml-6 mt-2 text-blue-400 animate-bounce" />
+            </div>
+            <div className="relative group">
+              <button
+                onClick={handleExploreSuggestion}
+                className="px-5 py-5 bg-gradient-to-tr from-transparent to-violet-500 rounded-lg shadow-lg hover:scale-105 hover:opacity-80 duration-300"
+              >
+                <Lightbulb className="w-8 h-8 text-yellow-200 mix-blend-multiply dark:mix-blend-screen" />
+              </button>
+              <div className="absolute left-1/2 transform -translate-x-1/2 top-full -mt-[30px] hidden group-hover:block">
+                <div className="relative bg-transparent text-yellow-800 font-thin dark:text-yellow-100 px-3 py-1.5 text-xs rounded-sm border border-yellow-300 dark:border-yellow-700 shadow-lg">
+                  <div className="absolute left-1/2 transform -translate-x-1/2 -top-1 w-2 h-2 bg-yellow-200 dark:bg-yellow-900 border-t border-l border-yellow-400 dark:border-yellow-700 rotate-45"></div>
+                  <span className="text-xs">Suggestions</span>
+                </div>
+              </div>
+              <ChevronsDown className="ml-6 mt-2 text-yellow-300 animate-bounce" />
+            </div>
           </div>
         </div>
       </div>
 
-      {/* Main Content */}
+      {/* Blog */}
       <section
-        ref={mainContentRef}
-        className="min-h-screen bg-violet-50 dark:bg-[#18181b] relative"
+        ref={blogRef}
+        className="min-h-screen bg-gradient-to-b from-violet-50 to-violet-200 dark:from-[#18181b] dark:to-black relative"
       >
         {/* Blog Posts */}
         <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
@@ -311,6 +221,24 @@ function Home() {
           </div>
         </div>
       </section>
+
+      {/* Lyric Card */}
+      <section
+        ref={lyricRef}
+        className="min-h-screen bg-violet-200 dark:bg-[#18181b] relative"
+      ></section>
+
+      {/* Playlist */}
+      <section
+        ref={playlistRef}
+        className="min-h-screen bg-violet-50 dark:bg-[#18181b] relative"
+      ></section>
+
+      {/* Suggestion */}
+      <section
+        ref={suggestionRef}
+        className="min-h-screen bg-violet-100 dark:bg-[#18181b] relative"
+      ></section>
     </div>
   );
 }
