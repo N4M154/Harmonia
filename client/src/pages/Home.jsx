ff5d71e0fd8a045226e3a3537903db402e62f76c
Namisa Najah <namisa.najah.raisa@gmail.com>
2025-02-11 15:40:21 +0600

    home:done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,6 +11,12 @@ import {
   Sparkles,
   MessageSquarePlus,
   TrendingUp,
+  Disc3,
+  Radio,
+  Music,
+  Search,
+  Palette,
+  Share2,
 } from "lucide-react";
 
 import { AnimatePresence, motion } from "framer-motion";
@@ -168,9 +174,9 @@ function Home() {
       >
         {/* Blog Posts */}
         <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
-          <div className="flex flex-col gap-6">
-            <h2 className="text-3xl font-light mt-20 mb-10 text-violet-700 text-center">
-              Recent Posts
+          <div className="flex flex-col gap-6 mt-20">
+            <h2 className="text-4xl md:text-5xl font-thin text-black/60 dark:text-violet-300 text-center mb-12">
+              Fresh Perspectives
             </h2>
             <div className="flex justify-center items-center gap-4">
               {/* Left Chevron */}
@@ -230,14 +236,181 @@ function Home() {
       {/* Lyric Card */}
       <section
         ref={lyricRef}
-        className="min-h-screen bg-violet-200 dark:bg-[#18181b] relative"
-      ></section>
+        className="min-h-screen bg-gradient-to-b from-violet-50 to-violet-100 dark:from-[#18181b] dark:to-black relative overflow-hidden"
+      >
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-10">
+          <div className="flex flex-col lg:flex-row items-start justify-between gap-12">
+            {/* Left Side - Example Card */}
+            <div className="w-full lg:w-1/2 xl:w-2/5">
+              <div className="sticky top-8">
+                <img
+                  src="/lyric.png"
+                  alt="Lyric Card Example"
+                  className="scale-75 rounded-xl shadow-[15px_15px_20px_rgba(0,0,0,1)] dark:shadow-[15px_15px_30px_rgba(255,255,255,0.2)] transform hover:scale-105 transition-all duration-300"
+                />
+              </div>
+            </div>
+
+            {/* Right Side - Content */}
+            <div className="w-full lg:w-1/2 xl:w-3/5 space-y-12 mt-[30px]">
+              {/* Hero Section */}
+              <div className="mb-12">
+                <div className="flex items-center gap-4 mb-6">
+                  <Music className="w-16 h-16 text-rose-400 dark:text-rose-300 animate-pulse" />
+                  <h1 className="text-4xl md:text-6xl font-thin text-black/60 dark:text-violet-300/70">
+                    Turn Lyrics into Art
+                  </h1>
+                </div>
+
+                <p className="text-lg text-gray-600 dark:text-gray-300 max-w-2xl font-thin">
+                  Create beautiful, shareable cards featuring your favorite song
+                  lyrics with our intuitive maker.
+                </p>
+              </div>
+              {/* Animated Background Shapes */}
+              <div className="absolute inset-0 overflow-hidden">
+                <div className="absolute top-1/5 left-1/2 w-96 h-96 bg-rose-400/10 dark:bg-rose-400/60 rounded-full mix-blend-multiply filter blur-xl animate-blob"></div>
+                <div className="absolute top-1/5 right-1 w-96 h-96 bg-orange-400/10 dark:bg-orange-400/60 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-2000"></div>
+                <div className="absolute bottom-1/4 left-1/3 w-96 h-96 bg-violet-500/10 dark:bg-violet-500/70 rounded-full mix-blend-multiply filter blur-xl animate-blob animation-delay-4000"></div>
+              </div>
+
+              {/* Features Grid */}
+              <div className="grid md:grid-cols-3 gap-4">
+                <div className="backdrop-blur-lg bg-white/10 dark:bg-white/5 border border-white/20 rounded-xl p-6 shadow-lg transform hover:-translate-y-1 transition-all duration-300">
+                  <div className="flex items-center justify-center w-12 h-12 bg-violet-100 dark:bg-violet-900/50 rounded-lg mb-4">
+                    <Search className="w-6 h-6 text-violet-600 dark:text-violet-400" />
+                  </div>
+                  <h3 className="text-xl font-semibold text-black dark:text-violet-300 mb-3">
+                    Find Your Song
+                  </h3>
+                  <p className="text-black dark:text-white font-thin">
+                    Search by song title, artist, or paste a Spotify link to
+                    instantly load your favorite track.
+                  </p>
+                </div>
+
+                <div className="backdrop-blur-lg bg-white/10 dark:bg-white/5 border border-white/20 rounded-xl p-6 shadow-lg transform hover:-translate-y-1 transition-all duration-300">
+                  <div className="flex items-center justify-center w-12 h-12 bg-violet-100 dark:bg-violet-900/50 rounded-lg mb-4">
+                    <Palette className="w-6 h-6 text-violet-600 dark:text-violet-400" />
+                  </div>
+                  <h3 className="text-xl font-semibold text-black dark:text-violet-300 mb-3">
+                    Customize Design
+                  </h3>
+                  <p className="text-black font-thin dark:text-white">
+                    Choose your perfect color scheme with our intuitive gradient
+                    and text color pickers.
+                  </p>
+                </div>
+
+                <div className="backdrop-blur-lg bg-white/10 dark:bg-white/5 border border-white/20 rounded-xl p-6 shadow-lg transform hover:-translate-y-1 transition-all duration-300">
+                  <div className="flex items-center justify-center w-12 h-12 bg-violet-100 dark:bg-violet-900/50 rounded-lg mb-4">
+                    <Share2 className="w-6 h-6 text-violet-600 dark:text-violet-400" />
+                  </div>
+                  <h3 className="text-xl font-semibold text-black dark:text-violet-300 mb-3">
+                    Share & Download
+                  </h3>
+                  <p className="text-black font-thin dark:text-white">
+                    Download your creation as a high-quality PNG image, perfect
+                    for sharing on social media.
+                  </p>
+                </div>
+              </div>
+
+              {/* CTA Section */}
+              <div className="text-center lg:text-left ml-[250px]">
+                <Link to="/lyric-card">
+                  <button className="px-8 py-2 hover:underline dark:hover:bg-opacity-70 underline-offset-2 rounded-full bg-rose-200 dark:bg-rose-400 hover:bg-opacity-70 text-rose-900 dark:text-rose-950 shadow-lg transform hover:-translate-y-0.5 transition-all duration-300">
+                    Create Your Lyric Card
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          {/* Decorative Elements */}
+          <div className="absolute top-20 left-10 w-20 h-20 bg-violet-800/40 dark:bg-violet-400/50 rounded-full blur-2xl dark:blur-3xl" />
+          <div className="absolute bottom-20 right-10 w-32 h-32 bg-rose-400/50 dark:bg-rose-400/40 rounded-full blur-2xl dark:blur-3xl" />
+        </div>
+      </section>
 
       {/* Playlist */}
       <section
         ref={playlistRef}
-        className="min-h-screen bg-violet-50 dark:bg-[#18181b] relative"
-      ></section>
+        className="min-h-screen bg-gradient-to-b from-violet-50 to-violet-200 dark:from-[#18181b] dark:to-black relative"
+      >
+        <div className="absolute inset-0 bg-grid-pattern opacity-5"></div>
+        <div className="max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8 py-12">
+          {/* Main content container */}
+          <div className="mt-10 grid lg:grid-cols-2 gap-12 items-center relative z-10">
+            {/* Left Column - Content */}
+            <div className="space-y-10">
+              {/* Header */}
+              <div className="space-y-6">
+                <Link to="/music">
+                  <div className="transition-all duration-300 shadow-xl dark:shadow-lg hover:underline dark:shadow-blue-400/50 hover:scale-110 cursor-pointer inline-flex items-center px-4 py-2 rounded-full bg-blue-200 dark:bg-blue-700/30 text-blue-800 dark:text-blue-200">
+                    <Disc3 className="w-5 h-5 mr-2 animate-spin" />
+                    <span className="text-sm font-semibold">
+                      Generate your Mood Music
+                    </span>
+                  </div>
+                </Link>
+                <h1 className="text-5xl font-semibold bg-gradient-to-r from-blue-500 to-violet-700 dark:from-blue-400 dark:to-violet-500 bg-clip-text text-transparent">
+                  Your Mood, Your Music
+                </h1>
+                <p className="text-md font-thin text-black dark:text-gray-300 max-w-xl">
+                  Transform your emotions into the perfect soundtrack. Select
+                  your current mood and let us craft a personalized playlist
+                  just for you.
+                </p>
+              </div>
+
+              {/* Feature Cards */}
+              <div className="grid grid-cols-2 gap-6">
+                <div className="p-6 rounded-2xl shadow-xl bg-white/40 dark:bg-white/5 backdrop-blur-sm border-t-4 border-r-4 border-violet-300 dark:border dark:border-violet-800/20 hover:shadow-2xl transition-all duration-300 hover:scale-105">
+                  <Radio className="w-8 h-8 text-violet-500 mb-4" />
+                  <h3 className="text-lg font-semibold text-gray-900 dark:text-violet-300">
+                    Smart Curation
+                  </h3>
+                  <p className="text-gray-600 dark:text-gray-400 text-sm mt-2">
+                    Playlist generation based on your emotional state powered by{" "}
+                    <span className="font-bold">
+                      Spotify for Developers API
+                    </span>
+                  </p>
+                </div>
+                <div className="p-6 rounded-2xl shadow-xl bg-white/40 dark:bg-white/5 backdrop-blur-sm border-t-4 border-r-4 border-violet-300 dark:border dark:border-violet-800/20 hover:shadow-2xl transition-all duration-300 hover:scale-105">
+                  <Sparkles className="w-8 h-8 text-violet-500 mb-4" />
+                  <h3 className="text-lg font-semibold text-gray-900 dark:text-violet-300">
+                    Instant Mix
+                  </h3>
+                  <p className="text-gray-600 dark:text-gray-400 text-sm mt-2">
+                    Get your perfect playlist in seconds with one-click
+                    generation
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* Right Column - Floating Playlist Image */}
+            <div className="ml-10 mt-20 relative hidden lg:block">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[100px] h-[100px] bg-gradient-to-r dark:from-violet-500 dark:to-fuchsia-500 rounded-full blur-3xl"></div>
+              <div className="relative">
+                <div className="w-[550px] h-[400px] absolute inset-0 bg-gradient-to-br from-violet-500/50 to-black/80 dark:from-violet-500/50 dark:to-black rounded-3xl transform rotate-6"></div>
+                <img
+                  src="/playlist.png"
+                  alt="Playlist Visualization"
+                  className=" w-[550px] h-[400px] object-contain rounded-3xl shadow-2xl transform -rotate-3 transition-transform hover:rotate-0 duration-500"
+                />
+                <div className="absolute -bottom-6 -right-6 w-32 h-32 bg-violet-500 dark:bg-violet-500 rounded-full blur-2xl opacity-40"></div>
+              </div>
+            </div>
+          </div>
+
+          {/* Decorative Elements */}
+          <div className="absolute top-20 left-10 w-32 h-32 bg-blue-500 dark:bg-blue-500/50 rounded-full blur-3xl"></div>
+          <div className="absolute bottom-20 right-10 w-48 h-48 bg-blue-500 dark:bg-blue-500/70 rounded-full blur-3xl"></div>
+        </div>
+      </section>
 
       {/* Suggestion */}
       <section
@@ -309,9 +482,9 @@ function Home() {
           </div>
 
           {/* CTA Section */}
-          <div className="mt-20 text-center">
+          <div className="mt-[40px] text-center">
             <Link to="/suggest">
-              <button className="px-8 py-4 bg-violet-800 hover:bg-violet-700 text-white rounded-lg shadow-lg transform hover:-translate-y-0.5 transition-all duration-300">
+              <button className="px-8 py-2 hover:underline underline-offset-2 bg-yellow-200 hover:opacity-70 dark:bg-yellow-400/70 text-yellow-900 dark:text-yellow-950 rounded-full shadow-lg transform hover:-translate-y-0.5 transition-all duration-300">
                 Share Your Suggestion
               </button>
             </Link>
